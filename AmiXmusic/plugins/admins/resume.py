961818b9cba3eb38006d2fdab15f94481ea1e5d3
KILLADI <118595331+Killadi-here@users.noreply.github.com>
2024-05-03 21:44:28 +0530

    Update resume.py

--- AmiXmusic/plugins/admins/resume.py
+++ AmiXmusic/plugins/admins/resume.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import is_music_playing, music_on
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from AmiXmusic import app
+from AmiXmusic.core.call import Anony
+from AmiXmusic.utils.database import is_music_playing, music_on
+from AmiXmusic.utils.decorators import AdminRightsCheck
+from AmiXmusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
