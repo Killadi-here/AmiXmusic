4c1a3e25e41de58d95a36584ff4ea0d10c4773d0
KILLADI <118595331+Killadi-here@users.noreply.github.com>
2024-05-03 21:46:22 +0530

    Update shuffle.py

--- AmiXmusic/plugins/admins/shuffle.py
+++ AmiXmusic/plugins/admins/shuffle.py
@@ -3,10 +3,10 @@ import random
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import db
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from AmiXmusic import app
+from AmiXmusic.misc import db
+from AmiXmusic.utils.decorators import AdminRightsCheck
+from AmiXmusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
