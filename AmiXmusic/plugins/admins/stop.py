b3f5127fc4d2e04405f73ba72b977f1d5c2fc28f
KILLADI <118595331+Killadi-here@users.noreply.github.com>
2024-05-03 21:50:56 +0530

    Update stop.py

--- AmiXmusic/plugins/admins/stop.py
+++ AmiXmusic/plugins/admins/stop.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import set_loop
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from AmiXmusic import app
+from AmiXmusic.core.call import Anony
+from AmiXmusic.utils.database import set_loop
+from AmiXmusic.utils.decorators import AdminRightsCheck
+from AmiXmusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
