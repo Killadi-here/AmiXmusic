33a65f585f1ce24bc1ede989d6c25bea5f58328a
KILLADI <118595331+Killadi-here@users.noreply.github.com>
2024-05-03 22:23:49 +0530

    Update stats.py

--- AmiXmusic/plugins/tools/stats.py
+++ AmiXmusic/plugins/tools/stats.py
@@ -9,13 +9,13 @@ from pyrogram.types import InputMediaPhoto, Message
 from pytgcalls.__version__ import __version__ as pytgver
 
 import config
-from AnonXMusic import app
-from AnonXMusic.core.userbot import assistants
-from AnonXMusic.misc import SUDOERS, mongodb
-from AnonXMusic.plugins import ALL_MODULES
-from AnonXMusic.utils.database import get_served_chats, get_served_users, get_sudoers
-from AnonXMusic.utils.decorators.language import language, languageCB
-from AnonXMusic.utils.inline.stats import back_stats_buttons, stats_buttons
+from AmiXmusic import app
+from AmiXmusic.core.userbot import assistants
+from AmiXmusic.misc import SUDOERS, mongodb
+from AmiXmusic.plugins import ALL_MODULES
+from AmiXmusic.utils.database import get_served_chats, get_served_users, get_sudoers
+from AmiXmusic.utils.decorators.language import language, languageCB
+from AmiXmusic.utils.inline.stats import back_stats_buttons, stats_buttons
 from config import BANNED_USERS
 
 
